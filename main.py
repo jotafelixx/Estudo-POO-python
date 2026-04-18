026b453f054c18491b2e517e93167d8f2a71c8a1
João Victor Santos <joaovictor.felixx@hotmail.com>
2026-04-17 23:10:13 -0300

    feat: add player action menu and improve combat loop logic

--- main.py
+++ main.py
@@ -6,15 +6,29 @@ inimigo = Inimigo()
 jogador = Player(nome_jogador)
 jogador.mostrar_status()
 inimigo.mostrar_status()
-while jogador.hp > 0 and inimigo.hp > 0:
-    print("\n--- NOVO TURNO ---")
-    dano = jogador.atacar()
-    dano_inimigo = inimigo.atacar()
-    inimigo.tomar_dano(dano)
-    jogador.tomar_dano(dano_inimigo)
-if inimigo.hp <= 0:
-    jogador.dinheiro += inimigo.recompensa
-    print(f'Você matou o inimigo de nivel {inimigo.nivel} e foi adicionado {inimigo.recompensa} na sua carteira você tem {jogador.dinheiro} agora!')
-elif jogador.hp == 0:
-    print("Você perdeu!:(")
+acao = 1
 
+    
+while acao == 1:
+    acao = int(input("\n--- NOVO TURNO ---\n1 - Atacar\n 2 - Fugir\n"))   
+    if acao != 1 and acao != 2:
+        print("Digite uma das ações!")
+        continue
+    if acao == 2:
+        break
+    elif acao == 1:
+        dano = jogador.atacar()
+        inimigo.tomar_dano(dano)
+    if inimigo.hp <= 0:
+        jogador.dinheiro += inimigo.recompensa
+        print(f'Você matou o inimigo de nivel {inimigo.nivel} e foi adicionado {inimigo.recompensa} na sua carteira você tem {jogador.dinheiro} agora!')
+    else:
+        dano_inimigo = inimigo.atacar()
+        jogador.tomar_dano(dano_inimigo)
+  
+    if jogador.hp <= 0:
+        print("Você perdeu!:(")
+        break
+
+
+    
